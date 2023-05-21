ad7291cad6ff99886ef4ac902f9d0405b7928b0e
MohamedKashif <mohamed.aslam@cassbana.com>
2023-05-21 23:39:19 +0500

    add angular env files

--- src/app/features/feature.service.ts
+++ src/app/features/feature.service.ts
@@ -2,12 +2,13 @@ import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
 import {Feature} from './feature';
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class FeatureService {
-  private apiUrl = 'http://localhost:3000/api/v1/features'; // Replace with your API endpoint
+  private apiUrl = `${environment.API_URL}/api/v1/features`;
 
   constructor(private http: HttpClient) {
   }
